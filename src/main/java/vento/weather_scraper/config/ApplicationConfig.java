bedb95990d766ce9465935d31e9bffab1bb470f3
VENTO <vento140@mail.ru>
2024-06-27 10:27:02 +0300

    Application properties were refactored

--- src/main/java/vento/weather_scraper/config/ApplicationConfig.java
+++ src/main/java/vento/weather_scraper/config/ApplicationConfig.java
@@ -1,36 +1,20 @@
 package vento.weather_scraper.config;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Configuration
+@Component
+@Getter
 public class ApplicationConfig {
+    @Value("${scheduler.delay}")
+    private Long schedulerDelay;
+
     @Value("${visual_crossing.token}")
     private String visualCrossingToken;
+
     @Value("${visual_crossing.coords}")
     private String visualCrossingCoords;
-
-    @Bean
-    public Gson gson() {
-        return new GsonBuilder().create();
-    }
-
-    /**
-     * Visual Crossing API token.
-     */
-    @Bean(name = "visualCrossingToken")
-    public String visualCrossingToken() {
-        return visualCrossingToken;
-    }
-
-    /**
-     * Visual Crossing coordinates.
-     */
-    @Bean(name = "visualCrossingCoords")
-    public String visualCrossingCoords() {
-        return visualCrossingCoords;
-    }
 }
--- src/main/java/vento/weather_scraper/service/impl/WeatherScraperServiceImpl.java
+++ src/main/java/vento/weather_scraper/service/impl/WeatherScraperServiceImpl.java
@@ -1,8 +1,8 @@
 package vento.weather_scraper.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import vento.weather_scraper.config.ApplicationConfig;
 import vento.weather_scraper.handler.WeatherApi;
 import vento.weather_scraper.handler.impl.VisualCrossingApi;
 import vento.weather_scraper.service.WeatherScraperService;
@@ -21,20 +21,16 @@ import java.util.concurrent.TimeUnit;
 @Component
 public class WeatherScraperServiceImpl implements WeatherScraperService {
     @Autowired
-    @Qualifier("visualCrossingToken")
-    private String visualCrossingToken;
-    @Autowired
-    @Qualifier("visualCrossingCoords")
-    private String visualCrossingCoords;
+    private ApplicationConfig config;
 
     private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
     private final List<WeatherApi> apiHandlers = new ArrayList<>();
 
     @PostConstruct
     private void init() {
-        apiHandlers.add(new VisualCrossingApi("visual_crossing", visualCrossingToken, visualCrossingCoords));
+        apiHandlers.add(new VisualCrossingApi("visual_crossing", config.getVisualCrossingToken(), config.getVisualCrossingCoords()));
 
-        scheduler.scheduleWithFixedDelay(this::fetchWeatherAPIs, 0, 15, TimeUnit.MINUTES);
+        scheduler.scheduleAtFixedRate(this::fetchWeatherAPIs, 0, config.getSchedulerDelay(), TimeUnit.MINUTES);
     }
 
     @Override
